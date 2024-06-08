5ad18a61f3132beb27a4cd3658ac4d38fea39d58
Stephan Fuchs <stephan@nicefuchs.fritz.box>
2024-06-08 23:39:11 +0200

    Kommentare in Exceloutput

--- PythonCode/mysite/polls/views.py
+++ PythonCode/mysite/polls/views.py
@@ -41,7 +41,7 @@ def ankommen_speichern(request, id):
     if len(Bereits_Eingeloggt)>0: # Bereits ein Eintrag vorhanden
         print("Eintrag bereits vorhanden.")
     else:
-        Datum=Anwesenheitsliste(qr_id=id,ankunft=aktuelle_zeit,verlassen=None,kommentar="Test",aufenthaltsdauer=0  )
+        Datum=Anwesenheitsliste(qr_id=id,ankunft=aktuelle_zeit,verlassen=None,kommentar=None,aufenthaltsdauer=0  )
         Datum.save()
 
         Person_Heutigeanmeldung=Person.objects.get(id=id)
@@ -159,7 +159,7 @@ def export_excel(request):
     Exportie die Anwesenheitsliste in eine Excel Datei
     """
     anwesenheits_eintraege = Anwesenheitsliste.objects.all()
-    personen = Person.objects.all()
+    #personen = Person.objects.all()
     print("---------------")
     #print("Personen bei Export Excel",personen)
     vornamen=[]
@@ -168,23 +168,28 @@ def export_excel(request):
     verlassen=[]
     klasse=[]
     differenz=[]
+    kommentar=[]
     for zeile in anwesenheits_eintraege:
         schueler=Person.objects.get(id=zeile.qr_id)
         nachnamen .append(schueler.nachname)
         vornamen  .append(schueler.vorname)
         klasse    .append(schueler.klasse)
+        kommentar .append(zeile.kommentar)
+
         ankunft   .append(zeile.ankunft.  strftime("%d.%m.%Y %H:%M"))
 
         if zeile.verlassen == None: # Vergessene Abmeldungen auf 18:00 gleichen Tag festlegen
             verlassen .append(zeile.ankunft.strftime("%d.%m.%Y 18:00"))
+            zeile.kommentar = "Abmeldung Automatisch"
+            zeile.save()
         else:
             verlassen .append(zeile.verlassen.strftime("%d.%m.%Y %H:%M"))
 
         delta=(datetime.strptime(verlassen[-1],"%d.%m.%Y %H:%M")-datetime.strptime(ankunft[-1],"%d.%m.%Y %H:%M"))
         differenz .append(round((delta).total_seconds()/60))
     print(nachnamen)
-    data={"Nachnamen":nachnamen,"Vornamen":vornamen,"Klasse":klasse,"Ankunft":ankunft,"Verlassen":verlassen,"DauerMinuten":differenz}
-    df1 = pd.DataFrame(data,columns=['Vornamen','Nachnamen',"Klasse",'Ankunft','Verlassen','DauerMinuten'])
+    data={"Nachnamen":nachnamen,"Vornamen":vornamen,"Klasse":klasse,"Ankunft":ankunft,"Verlassen":verlassen,"DauerMinuten":differenz,"Kommentar":kommentar}
+    df1 = pd.DataFrame(data,columns=['Vornamen','Nachnamen',"Klasse",'Ankunft','Verlassen','DauerMinuten','Kommentar'])
     two_up = Path(__file__).resolve().parents[1]
     print(two_up)
     df1.to_excel(str(two_up)+"/static/output.xlsx",index=False)  # download button
@@ -206,7 +211,7 @@ def import_excel(request):
         print("-------------")
         print("Row",row)
         print("vorname",row["Vorname"],"nachname",row["Nachname"],"klasse",row["Klasse"])
-        Personen_Stand=Person(id=index,vorname=row["Vorname"],nachname=row["Nachname"],klasse=row["Klasse"],qr_id=index,ankunft="2023-01-01 12:00:00.000000")
+        Personen_Stand=Person(id=index,vorname=row["Vorname"],nachname=row["Nachname"],klasse=row["Klasse"],qr_id=index,ankunft=None)
     Personen_Stand.save()
     return render(request, 'htmlseiten/excelimport.html', {'data': Personen_Stand })
 
